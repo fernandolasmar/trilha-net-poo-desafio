b42ea91b822b229dc2e17940a93522aa65590606
Fernando Lasmar <f3rnandolasmar@gmail.com>
2022-10-26 17:44:08 -0300

    Desafio POO sistema celular - classes smartphone, iphone e nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "101010", modelo: "Iphone teste", imei: "12345", memoria: 128);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("------------------------------------------------");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "101010", modelo: "Nokia teste", imei: "7410", memoria: 64);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
